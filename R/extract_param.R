20bda9658ceded2f9f46c946154274a93d47fb21
adamkucharski <adam.kucharski@lshtm.ac.uk>
2022-04-29 22:18:02 +0100

    Add extract parameter optim

--- R/extract_param.R
+++ R/extract_param.R
@@ -2,7 +2,7 @@
 #'
 #' Extract parameters from summary data (e.g. in meta analysis)
 #' @param type A \code{character} defining whether summary statistics based around \code{median} and 95% CI (default) or \code{mean} and variance.
-#' @param values A \code{vector} with three elements if \code{type=median}: c(median, lower 25% CI, upper 75% CI); and with two elements if \code{type=mean}: c(mean, variance)
+#' @param values A \code{vector} with two elements. If \code{type=median}: c(median, upper 75% CI); and if \code{type=mean}: c(mean, variance)
 #' @param distribution A \code{character} specifying distribution to use. Default is \code{lognormal}; also takes \code{gamma}. 
 #' @keywords extract
 #' @export
@@ -11,10 +11,27 @@
 
 extract_param <- function(type,values,distribution){
   
+  # DEBUG:
+  # type = "median"; values = c(3.2,3.5); distribution = "lnorm"
+  
   # Validate inputs
-  if(type=="median" & length(values)!=3){stop("Need 'values' to be vector length 3")}
-  if(type=="mean" & length(values)!=2){stop("Need 'values' to be vector length 2")}
+  #if(type=="median" & length(values)!=3){stop("Need 'values' to be vector length 3")}
+  if(length(values)!=2){stop("Need 'values' to be vector length 2")}
 
   # Extract distribution parameters using optimisation
   
+  param = c(a = 1, b = 1)
+  result2 = optim(param, fit_function, method="L-BFGS-B", val=values, lower=c(0,0), hessian=FALSE) #, control=list(trace=1))
+  
+  
 }
+
+#' @rdname fit_function
+fit_function <- function(param, val) {
+
+  (plnorm(val[1], meanlog = param[["a"]],sdlog = param[["b"]]) - 0.5)^2 + (plnorm(val[2], meanlog = param[["a"]],sdlog = param[["b"]]) - 0.75)^2
+  
+}
+
+# TO DO - add function to fit to raw data?
+
