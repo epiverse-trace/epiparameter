3142eb78ba9591c933516b404f1a84a3a5fd65a4
adamkucharski <adam.kucharski@lshtm.ac.uk>
2022-05-04 20:04:19 +0100

    Add parameter option to list_distributions

--- R/list_distributions.R
+++ R/list_distributions.R
@@ -2,16 +2,21 @@
 #'
 #' List distributions included in package
 #' @param type A \code{character} defining parameter to be listed
+#' @param parameters A \code{logical} defining whether to show parameter values
 #' @keywords distributions
 #' @export
 #' @examples
 #' list_distributions()
 
-list_distributions <- function(type="incubation"){
+list_distributions <- function(type="incubation",parameters=F){
   
   if(type=="incubation"){
-    output <- incubation_vals %>% select(pathogen_ID,study_ID,year,size,distribution)
+    show_values <- incubation_vals
   }
+  
+  if(parameters==T){ output <- show_values}
+  if(parameters==F){ output <- show_values  %>% select(pathogen_ID,study_ID,year,size,distribution)}
+  
   output
   
 }
