ddc9ecdcffb4c5b420db2848a4c68ac9a23ef156
adamkucharski <adam.kucharski@lshtm.ac.uk>
2022-05-04 23:08:46 +0100

    Clean up dependencies

--- R/prob_f.R
+++ R/prob_f.R
@@ -16,11 +16,11 @@ prob_f <- function(pathogen, type, study = NULL, pmf = T){
   # pathogen="influenza_H7N9"; type="incubation"; study=NULL; pmf = T
   
   # Extract pathogen and parameter type
-  pick_path <- param_vals %>% filter(pathogen_ID==pathogen & type_ID==type)  
+  pick_path <- param_vals %>% dplyr::filter(pathogen_ID==pathogen & type_ID==type)  
 
   # Extract study or default to largest sample size
-  if( is.null(study) ){pick_study <- pick_path %>% filter(size==max(size))}
-  if(!is.null(study) ){pick_study <- pick_path %>% filter(study_ID==study)}
+  if( is.null(study) ){pick_study <- pick_path %>% dplyr::filter(size==max(size))}
+  if(!is.null(study) ){pick_study <- pick_path %>% dplyr::filter(study_ID==study)}
 
   # Define distribution
   if(pick_study$distribution=="lnorm"){
