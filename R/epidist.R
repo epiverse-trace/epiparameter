87d8c4a73f9937a0afa1527b14ade28d3ff9235d
adamkucharski <adam.kucharski@lshtm.ac.uk>
2022-07-20 22:41:24 +0100

    Update params
    Update data and parameter extraction functions

--- R/epidist.R
+++ R/epidist.R
@@ -4,19 +4,20 @@
 #' @param pathogen A \code{character} specifying pathogen of interest
 #' @param pathogen A \code{character} specifying type of parameter: "incubation", "onset_to_admission", "onset_to_death"
 #' @param study A \code{character} specifying dataset to use. Defaults to study with largest sample size.
-#' @param pmf A \code{logical} specifying whether to output probability mass function (\code{TRUE}) or cumulative mass function (\code{FALSE}). Defaults to \code{TRUE}.
 #' @keywords incubation
 #' @export
 #' @examples
 #' epidist()
 
-epidist  <- function(pathogen, type, study = NULL, pmf = T){
+epidist  <- function(pathogen, type, study = NULL){
   
   # DEBUG
-  # pathogen="influenza_H7N9"; type="incubation"; study=NULL; pmf = T
+  # pathogen="SARS1"; type="incubation"; study=NULL; pmf = T
   
   # Extract pathogen and parameter type
   pick_path <- param_vals %>% dplyr::filter(pathogen_ID==pathogen & type_ID==type)  
+  
+  if(nrow(pick_path)==0){stop("Need to select pathogen and distribution in the dataset")}
   
   # Extract study or default to largest sample size
   if( is.null(study) ){pick_study <- pick_path %>% dplyr::filter(size==max(size))}
@@ -25,21 +26,32 @@ epidist  <- function(pathogen, type, study = NULL, pmf = T){
   # Define distribution
   if(pick_study$distribution=="lnorm"){
     param_vector <- c(meanlog = pick_study$meanlog, sdlog = pick_study$sdlog)
-    cmf_function <- function(x){plnorm(x,meanlog = pick_study$meanlog,sdlog = pick_study$sdlog)}
+    cdf_function <- function(x){plnorm(x,meanlog = pick_study$meanlog,sdlog = pick_study$sdlog)}
     pmf_function <- function(x){cmf_function(x+1)-cmf_function(x)} 
+    pdf_function <- function(x){dlnorm(x,meanlog = pick_study$meanlog,sdlog = pick_study$sdlog)} 
   }
   
   if(pick_study$distribution=="gamma"){
     param_vector <- c(shape = pick_study$shape,scale = pick_study$scale)
-    cmf_function <- function(x){pgamma(x,shape = pick_study$shape,scale = pick_study$scale)}
+    cdf_function <- function(x){pgamma(x,shape = pick_study$shape,scale = pick_study$scale)}
     pmf_function <- function(x){cmf_function(x+1)-cmf_function(x)} 
+    pdf_function <- function(x){dgamma(x,shape = pick_study$shape,scale = pick_study$scale)} 
   }
   
-  out <- list(dist = pick_study$distribution,
+  if(pick_study$distribution=="weibull"){
+    param_vector <- c(shape = pick_study$shape,scale = pick_study$scale)
+    cdf_function <- function(x){pweibull(x,shape = pick_study$shape,scale = pick_study$scale)}
+    pmf_function <- function(x){cmf_function(x+1)-cmf_function(x)} 
+    pdf_function <- function(x){dweibull(x,shape = pick_study$shape,scale = pick_study$scale)} 
+  }
+  
+  out <- list(pathogen = pathogen,
+              dist = pick_study$distribution,
               type = type,
               param = param_vector,
               pmf = pmf_function,
-              cmf = cmf_function)
+              pdf = pdf_function,
+              cdf = cdf_function)
   
   class(out) <- "epidist"
   out
@@ -49,8 +61,9 @@ epidist  <- function(pathogen, type, study = NULL, pmf = T){
 ##' @export
 print.epidist <- function(x, ...){
 
-  cat(sprintf("Distribution: %s\n", x$distn))
+  cat(sprintf("Pathogen: %s\n", x$pathogen))
   cat(sprintf("Type: %s\n", x$type))
+  cat(sprintf("Distribution: %s\n", x$dist))
   p_vals <- x$param
   cat(sprintf("Parameters:\n"))
   cat(sprintf("  %s: %s\n",names(p_vals),as.character(p_vals)))
--- R/extract_param.R
+++ R/extract_param.R
@@ -1,36 +1,64 @@
 #' Extract parameters
 #'
 #' Extract parameters from summary data (e.g. in meta analysis)
-#' @param type A \code{character} defining whether summary statistics based around \code{median} and 95% CI (default) or \code{mean} and variance.
-#' @param values A \code{vector} with two elements. If \code{type=median}: c(median, upper 75% CI); and if \code{type=mean}: c(mean, variance)
-#' @param distribution A \code{character} specifying distribution to use. Default is \code{lognormal}; also takes \code{gamma}. 
+#' @param type A \code{character} defining whether summary statistics based around \code{median} and 95% quantile (default) or \code{mean} and variance.
+#' @param values A \code{vector} with two elements. If \code{type=median}: c(median, upper quantile); and if \code{type=mean}: c(mean, variance)
+#' @param distribution A \code{character} specifying distribution to use. Default is \code{lognormal}; also takes \code{gamma} and \code{weibull}.
+#' @param percentiles A \code{vector} with two elements specifying the quantiles to compare to if using \code{type="median"}.
 #' @keywords extract
 #' @export
 #' @examples
 #' extract_param()
 
-extract_param <- function(type,values,distribution){
+extract_param <- function(type="median",
+                          values,
+                          distribution,
+                          percentiles=c(0.5,0.95)){ # percentile range
   
   # DEBUG:
-  # type = "median"; values = c(0.6,0.7); distribution = "lnorm"
+  # type = "median"; values = c(5.9,21.4); distribution = "lnorm"; percentiles = c(0.025,0.975)
   
   # Validate inputs
   #if(type=="median" & length(values)!=3){stop("Need 'values' to be vector length 3")}
   if(length(values)!=2){stop("Need 'values' to be vector length 2")}
 
-  # Extract distribution parameters using optimisation
-  param = c(a = 1, b = 1)
-  result2 = optim(param, fit_function, method="L-BFGS-B", val=values, lower=c(-1,0), hessian=FALSE) #, control=list(trace=1))
+  # Extract distribution parameters using optimisation for specific distribution
+  param = c(a = 2, b = 0.5) # CHECK: numerical stability
+  values_in <- c(values,q1 = percentiles[1], q2 = percentiles[2])
   
+  if(distribution == "lnorm"){
+    result2 = optim(param, fit_function_lnorm, method="L-BFGS-B", val=values_in, lower=c(-1,0))
+  }
+  if(distribution == "gamma"){
+    result2 = optim(param, fit_function_gamma, method="L-BFGS-B", val=values_in, lower=c(-1,0))
+  }
+  if(distribution == "weibull"){
+    result2 = optim(param, fit_function_weibull, method="L-BFGS-B", val=values_in, lower=c(-1,0))
+  }
+
   # Output parameters
   result2$par
   
 }
 
-#' @rdname fit_function
-fit_function <- function(param, val) {
+#' @rdname fit_function_lnorm
+fit_function_lnorm <- function(param, val) {
 
-  (plnorm(val[1], meanlog = param[["a"]],sdlog = param[["b"]]) - 0.5)^2 + (plnorm(val[2], meanlog = param[["a"]],sdlog = param[["b"]]) - 0.75)^2
+  (plnorm(val[1], meanlog = param[["a"]],sdlog = param[["b"]]) - val[["q1"]])^2 + (plnorm(val[2], meanlog = param[["a"]],sdlog = param[["b"]]) - val[["q2"]])^2
+  
+}
+
+#' @rdname fit_function_gamma
+fit_function_gamma <- function(param, val) {
+  
+  (pgamma(val[1], shape = param[["a"]],scale = param[["b"]]) - val[["q1"]])^2 + (pgamma(val[2], shape = param[["a"]],scale = param[["b"]]) - val[["q2"]])^2
+  
+}
+
+#' @rdname fit_function_weibull
+fit_function_weibull <- function(param, val) {
+  
+  (pweibull(val[1], shape = param[["a"]],scale = param[["b"]]) - val[["q1"]])^2 + (pweibull(val[2], shape = param[["a"]],scale = param[["b"]]) - val[["q2"]])^2
   
 }
 
