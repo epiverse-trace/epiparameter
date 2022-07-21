8e007e8b671c34b33b7e568681c93b9168c4954f
adamkucharski <adam.kucharski@lshtm.ac.uk>
2022-07-21 22:32:14 +0100

    Add extraction from range

--- R/extract_param.R
+++ R/extract_param.R
@@ -1,43 +1,129 @@
 #' Extract parameters
 #'
 #' Extract parameters from summary data (e.g. in meta analysis)
-#' @param type A \code{character} defining whether summary statistics based around \code{median} and 95% quantile (default) or \code{mean} and variance.
-#' @param values A \code{vector} with two elements. If \code{type=median}: c(median, upper quantile); and if \code{type=mean}: c(mean, variance)
-#' @param distribution A \code{character} specifying distribution to use. Default is \code{lognormal}; also takes \code{gamma} and \code{weibull}.
-#' @param percentiles A \code{vector} with two elements specifying the quantiles to compare to if using \code{type="median"}.
+#' @param type A \code{character} defining whether summary statistics based around \code{percentiles} (default) or \code{range}
+#' @param values A \code{vector}. If \code{type=percentiles}: c(percentile_1, percentile_2); and if \code{type=range}: c(median, min, max)
+#' @param distribution A \code{character} specifying distribution to use. Default is \code{lnorm}; also takes \code{gamma} and \code{weibull}.
+#' @param percentiles A \code{vector} with two elements specifying the percentiles defined in \code{values} if using \code{type="percentiles"}.
+#' @param samples A \code{numeric} specifying the sample size if using \code{type="range"}.
 #' @keywords extract
 #' @export
 #' @examples
 #' extract_param()
 
-extract_param <- function(type="median",
+extract_param <- function(type="percentiles",
                           values,
-                          distribution,
-                          percentiles=c(0.5,0.95)){ # percentile range
+                          distribution="lnorm",
+                          percentiles,
+                          samples){ 
   
   # DEBUG:
-  # type = "median"; values = c(5.9,21.4); distribution = "lnorm"; percentiles = c(0.025,0.975)
+  # type = "range"; values = c(7,3,20); distribution = "lnorm"; samples=20
   
   # Validate inputs
-  #if(type=="median" & length(values)!=3){stop("Need 'values' to be vector length 3")}
-  if(length(values)!=2){stop("Need 'values' to be vector length 2")}
-
-  # Extract distribution parameters using optimisation for specific distribution
-  param = c(a = 2, b = 0.5) # CHECK: numerical stability
-  values_in <- c(values,q1 = percentiles[1], q2 = percentiles[2])
+  if(type=="percentiles"){ if(length(values)!=2){stop("Need 'values' to be vector length 2")} }
+  if(type=="range"){ if(length(values)!=3){stop("Need 'values' to be vector length 3")} }
+  if(type=="percentiles"){ if(length(percentiles)!=2){stop("Need 'percentiles' to be vector length 2")} }
   
-  if(distribution == "lnorm"){
-    result2 = optim(param, fit_function_lnorm, method="L-BFGS-B", val=values_in, lower=c(-1,0))
+  # Percentile extraction
+  # Extract distribution parameters using optimisation for specific distribution
+  if(type=="percentiles"){
+    
+    # Set initial values for optimisation
+    param = c(a = 2, b = 0.5) # CHECK: numerical stability
+    values_in <- c(values,q1 = percentiles[1], q2 = percentiles[2])
+    
+    if(distribution == "lnorm"){
+      result2 = optim(param, fit_function_lnorm, method="L-BFGS-B", val=values_in, lower=c(-1,0))
+    }
+    if(distribution == "gamma"){
+      result2 = optim(param, fit_function_gamma, method="L-BFGS-B", val=values_in, lower=c(-1,0))
+    }
+    if(distribution == "weibull"){
+      result2 = optim(param, fit_function_weibull, method="L-BFGS-B", val=values_in, lower=c(-1,0))
+    }
+  
   }
-  if(distribution == "gamma"){
-    result2 = optim(param, fit_function_gamma, method="L-BFGS-B", val=values_in, lower=c(-1,0))
-  }
-  if(distribution == "weibull"){
-    result2 = optim(param, fit_function_weibull, method="L-BFGS-B", val=values_in, lower=c(-1,0))
+  
+  # Range extraction
+  
+  if(type=="range"){
+    
+    # Set initial values for optimisation
+    param = c(a = 2, b = 0.5) # CHECK: numerical stability
+    values_in <- c(values,n = samples)
+    
+    if(distribution == "lnorm"){
+      result2 = optim(param, fit_function_lnorm_range, method="L-BFGS-B", val=values_in, lower=c(-1,0))
+    }
+    if(distribution == "gamma"){
+      result2 = optim(param, fit_function_gamma_range, method="L-BFGS-B", val=values_in, lower=c(-1,0))
+    }
+    if(distribution == "weibull"){
+      result2 = optim(param, fit_function_weibull_range, method="L-BFGS-B", val=values_in, lower=c(-1,0))
+    }
+    
   }
 
   # Output parameters
   result2$par
+  
+}
+
+#' @rdname fit_function_lnorm_range
+fit_function_lnorm_range <- function(param, val) {
+  
+  # Median square residual
+  median_sr <- (plnorm(val[1], meanlog = param[["a"]],sdlog = param[["b"]]) - 0.5)^2 
+
+  # Probability of obtaining min, max and range:
+  min_p <- dlnorm(val[2], meanlog = param[["a"]],sdlog = param[["b"]])
+  max_p <- dlnorm(val[3], meanlog = param[["a"]],sdlog = param[["b"]])
+  range_p <- (plnorm(val[3], meanlog = param[["a"]],sdlog = param[["b"]]) - plnorm(val[2], meanlog = param[["a"]],sdlog = param[["b"]]))^(val[["n"]]-2)
+  
+  # Range likelihood
+  range_sr <- -(min_p*max_p*range_p)
+  
+  # Total value to be minimised
+  range_sr + median_sr 
+  
+}
+
+#' @rdname fit_function_gamma_range
+fit_function_gamma_range <- function(param, val) {
+  
+  # Median square residual
+  median_sr <- (pgamma(val[1], meanlog = param[["a"]],scale = param[["b"]]) - 0.5)^2 
+  
+  # Probability of obtaining min, max and range:
+  min_p <- dgamma(val[2], shape = param[["a"]],scale = param[["b"]])
+  max_p <- dgamma(val[3], shape = param[["a"]],scale = param[["b"]])
+  range_p <- (pgamma(val[3], shape = param[["a"]],scale = param[["b"]]) - pgamma(val[2], shape = param[["a"]],scale = param[["b"]]))^(val[["n"]]-2)
+  
+  # Range likelihood
+  range_sr <- -(min_p*max_p*range_p)
+  
+  # Total value to be minimised
+  range_sr + median_sr 
+  
+}
+
+#' @rdname fit_function_weibull_range
+fit_function_weibull_range <- function(param, val) {
+  
+  # Median square residual
+  median_sr <- (pweibull(val[1], shape = param[["a"]],scale = param[["b"]]) - 0.5)^2 
+  
+  # Probability of obtaining min, max and range:
+  min_p <- dweibull(val[2], shape = param[["a"]],scale = param[["b"]])
+  max_p <- dweibull(val[3], shape = param[["a"]],scale = param[["b"]])
+  range_p <- (pweibull(val[3], shape = param[["a"]],scale = param[["b"]]) - pweibull(val[2], scale = param[["a"]],scale = param[["b"]]))^(val[["n"]]-2)
+  
+  # Range likelihood
+  range_sr <- -(min_p*max_p*range_p)
+  
+  # Total value to be minimised
+  range_sr + median_sr 
   
 }
 
@@ -62,5 +148,4 @@ fit_function_weibull <- function(param, val) {
   
 }
 
-# TO DO - add function to fit to raw data?
 
