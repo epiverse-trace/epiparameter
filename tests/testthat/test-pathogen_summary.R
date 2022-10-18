02833dff5ca9e826817a4c28ce3dc0560d5b4b85
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2022-10-18 16:29:12 +0100

    Update tests/testthat/test-pathogen_summary.R
    Co-authored-by: Hugo Gruson <Bisaloo@users.noreply.github.com>

--- tests/testthat/test-pathogen_summary.R
+++ tests/testthat/test-pathogen_summary.R
@@ -1,6 +1,6 @@
 test_that("pathogen_summary works with valid input", {
   ebola_summary <- pathogen_summary(pathogen = "ebola")
-  expect_true(is.data.frame(ebola_summary))
+  expect_s3_class(ebola_summary, "data.frame")
   expect_equal(nrow(ebola_summary), 3)
   expect_equal(ncol(ebola_summary), 7)
   expect_equal(
