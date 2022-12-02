b80a11f90a86cf71e73034d3f308cbec253f6d04
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2022-12-02 17:39:15 +0000

    added functions that convert database and yaml to JSON data and schema and added suggested packages

--- R/utils.R
+++ R/utils.R
@@ -36,3 +36,82 @@ calc_disc_dist_quantile <- function(prob, days, quantile) {
   names(quantiles) <- as.character(quantile)
   quantiles
 }
+#' Creates a JSON version of the epiparameter database
+#'
+#' @description This converts the database from a data frame (read from the csv
+#' stored in the package) into a JSON data file and saves it. This functions
+#' allows the database to be edited and then the JSON file can be updated but
+#' calling this function.
+#'
+#' @param read_path A relative file path from the package root directory to read
+#' the parameters database from
+#' @param write_path A relative file path from the package root directory to
+#' write the JSON data file to
+#'
+#' @return Invisibly returns the JSON data
+#' @keywords internal
+make_json_data <- function(read_path = "extdata/parameters.csv",
+                           write_path = "inst/extdata/data.json") {
+  # read in epiparameter database
+  data <- read.csv(
+    file = system.file(
+      read_path,
+      package = "epiparameter",
+      mustWork = TRUE
+    ), header = TRUE
+  )
+
+  # convert cells with arrays to numeric
+  for (i in grep(pattern = "_ci$", x = colnames(data))) {
+    if (!all(is.na(data[[i]]))) {
+      data[[i]] <- lapply(strsplit(x = data[[i]], split = ","), as.numeric)
+    }
+
+  }
+
+  # convert data to json
+  data_json <- jsonlite::toJSON(data, dataframe = "columns", na = "null")
+  jsonlite::validate(data_json)
+
+  # save json data
+  jsonlite::write_json(data_json, path = write_path)
+
+  # invisibly return json data
+  invisible(data_json)
+}
+
+#' Creates a data schema in JSON format from a yaml file
+#'
+#' @description This converts the yaml data schema (data dictionary) stored in
+#' the package into a JSON schema and saves it. This functions allows the yaml
+#' to be the file which can be read and edited and then this file updates to
+#' the JSON schema to match the yaml schema.
+#'
+#' @param read_path A relative file path from the package root directory to read
+#' the yaml schema from
+#' @param write_path A relative file path from the package root directory to
+#' write the JSON schema to
+#'
+#' @return Invisibly returns the JSON schema
+#' @keywords internal
+make_json_schema <- function(read_path = "extdata/data_dictionary.yaml",
+                             write_path = "inst/extdata/schema.json") {
+  # read schema
+  schema <- yaml::read_yaml(
+    system.file(
+      read_path,
+      package = "epiparameter",
+      mustWork = TRUE
+    )
+  )
+
+  # convert schema to json
+  schema_json <- jsonlite::toJSON(schema, auto_unbox = TRUE)
+  jsonlite::validate(schema_json)
+
+  # save json schema
+  jsonlite::write_json(schema_json, path = write_path)
+
+  # invisibly return json schema
+  invisible(schema_json)
+}
