63b20ff2187cd8e197a82affbe27135e86e1d7b9
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-01-27 12:52:47 +0000

    added bind_epiparam() function and tests

---
+++ R/bind_epiparam.R
@@ -0,0 +1,68 @@
+#' Binds any epi data class in `{epiparameter}` (`epidist`, `vb_epidist`,
+#' `epiparam`) or data frame to an `epiparam` object.
+#'
+#' @details The `epiparam` class holds the library of epidemiological parameters
+#' that is stored in the `{epiparameter}` R package and can be manipulated. The
+#' `bind_epiparam()` function allows users to add entries to the library by
+#' binding them to the bottom of an existing `epiparam` object loaded in R.
+#'
+#' @param epiparam An `epiparam` object
+#' @param epi_obj Either an `epidist`, `vb_epidist`, `epiparam` or list of
+#' `epidist` objects. It can also be a data frame as long as the columns conform
+#' to the columsn of an `epiparam` object.
+#'
+#' @return An `epiparam` object
+#' @export
+#'
+#' @examples
+#' eparam <- epiparam()
+#' edist <- epidist(
+#'   disease = "ebola",
+#'   epi_dist = "incubation_period",
+#'   prob_distribution = "lognormal",
+#'   prob_distribution_params = c(meanlog = 1, sdlog = 1)
+#' )
+#' bind_epiparam(eparam, edist)
+bind_epiparam <- function(epiparam, epi_obj) {
+
+  # check input
+  validate_epiparam(epiparam)
+  if (inherits(epi_obj, "epidist")) {
+    validate_epidist(epi_obj)
+  } else if (inherits(epi_obj, "list")) {
+    lapply(epi_obj, validate_epidist)
+  } else if (inherits(epi_obj, "vb_epidist")) {
+    validate_vb_epidist(epi_obj)
+  } else if (inherits(epi_obj, "epiparam")) {
+    validate_epiparam(epi_obj)
+    # epiparam objects can be directly binded
+    out <- rbind(epiparam, epi_obj)
+    # validate new epiparam object
+    validate_epiparam(out)
+    return(out)
+  } else if (inherits(epi_obj, "data.frame")) {
+    stopifnot(
+      "data frame provided must have the same column names as epiparam" =
+      identical(colnames(epi_obj), colnames(epiparam))
+    )
+    # data frames can be directly binded
+    out <- rbind(epiparam, epi_obj)
+    # validate new epiparam object
+    validate_epiparam(out)
+    return(out)
+  } else {
+    stop("Only epidist, vb_epidist or epiparam can bind to epiparam")
+  }
+
+  # convert epidist to epiparam object
+  eparam <- as_epiparam(x = epi_obj)
+
+  # bind epidist to epi
+  out <- rbind(epiparam, eparam)
+
+  # validate new epiparam object
+  validate_epiparam(out)
+
+  # return new epiparam object
+  out
+}
