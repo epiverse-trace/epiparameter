51abe0fc3cbd182ce5ce82a2030afe9ea4ae90a9
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-01-27 12:43:03 +0000

    added prob_dist_params argument to calc_dist_params, updated calc_dist_params documentation, changed sample_size default to NA, added input checking

--- R/calc_dist_params.R
+++ R/calc_dist_params.R
@@ -1,18 +1,51 @@
 #' Calculates the parameters of a probability distribution from a list of
-#' available summary statistics.
+#' available summary statistics
 #'
-#' @param prob_dist A character string with the name of the probability
-#' distribution
-#' @param summary_stats A list of summary statistics. The structure of the list
-#' can be easily made with the helper function `create_epidist_summary_stats()`.
+#' @description When the parameters of a probability distribution are not
+#' provided (e.g. when describing the distribution in the literature) and instead
+#' summary statistics of a distribution are provided, the parameters can usually
+#' be calculated from the summary statistics. The `calc_dist_params()` function
+#' computes the parameters for the gamma, lognormal and weibull distributions.
+#'
+#' This function can provide a convinient wrapper around the conversion and
+#' extraction functions it is not known which summary statistics can be used to
+#' calculate the parameters of a distribution.
+#'
+#' @details The hierarchy of methods are: conversion is prioritised if the mean
+#' and standard deviation are available as these are mostly analytical
+#' conversions (except for one of the weibull conversions), next method if
+#' possible is extraction from percentiles. This method requires a lower
+#' percentile (between(0-50]) and an upper percentile(between (50-100)). If
+#' multiple percentiles in each of these ranges is provided the lowest value is
+#' used for the calculation. The last method is the extraction using a median
+#' and range of the data.
+#'
+#' @inheritParams new_epidist
+#' @inheritParams epidist
 #' @param sample_size The sample size of the data. Only needed when falling back
 #' on using the median-range extraction calculation.
 #'
 #' @return A named vector with parameters
 #' @export
 calc_dist_params <- function(prob_dist,
+                             prob_dist_params,
                              summary_stats,
-                             sample_size = NULL) {
+                             sample_size = NA) {
+
+  # check input
+  checkmate::assert_character(prob_dist, len = 1)
+  checkmate::assert_list(
+    summary_stats,
+    types = c("list", "double", "null"),
+    names = "unique"
+  )
+  checkmate::assert_number(sample_size, na.ok = TRUE)
+  stopifnot(
+    "probability distribution params must be a named vector or NA" =
+      all(is.na(prob_dist_params)) ||
+      !is.null(names(prob_dist_params))
+  )
+
   # extract mean and sd to see if conversion is possible
   mean_sd <- c(
     mean = summary_stats$central_tendency_spread$mean,
@@ -20,10 +53,9 @@ calc_dist_params <- function(prob_dist,
   )
 
   # extract quantiles of distribution to calculate parameters as second choice
-  percentile_values <- c(
-    unlist(summary_stats$quantiles[c("q_025", "q_05", "q_25", "q_50")]),
-    unlist(summary_stats$quantiles[c("q_75", "q_875", "q_95", "q_975")])
-  )
+  quantiles <- unlist(summary_stats$quantiles)
+  lower_quantiles <- quantiles[c("q_025", "q_05", "q_25", "q_50")]
+  upper_quantiles <- quantiles[c("q_75", "q_875", "q_95", "q_975")]
 
   # extract median and range to calculate parameters as third choice
   median_range <- c(
@@ -31,46 +63,49 @@ calc_dist_params <- function(prob_dist,
     unlist(summary_stats$range)
   )
 
-  if (length(mean_sd) == 2) {
+  if (!any(is.na(mean_sd))) {
     # make mean_sd a class with the name of the prob dist for multiple dispatch
-    class(summary_stats) <- prob_dist
-    params <- convert_params(summary_stats = summary_stats)
-  } else if (length(percentile_values) == 2) {
-    # use the first quantile in the set to calculate the parameters
-    lower_percentile <- unlist(
-      summary_stats$quantiles[c("q_025", "q_05", "q_25", "q_50")]
-    )[1]
-    upper_percentile <- unlist(
-      summary_stats$quantiles[c("q_75", "q_875", "q_95", "q_975")]
-    )[1]
+    prob_dist_params <- convert_params(
+      summary_stats = summary_stats,
+      prob_dist = prob_dist
+    )
+  } else if (!all(is.na(lower_quantiles)) && !all(is.na(upper_quantiles))) {
+    # use the first numeric quantile in the set to calculate the parameters
+    lower_percentile <- lower_quantiles[!is.na(lower_quantiles)][1]
+    upper_percentile <- upper_quantiles[!is.na(upper_quantiles)][1]
 
     # extract the numeric quantiles from the vector names
-    percentiles <- as.numeric(
-      gsub(pattern = "q_", replacement = "0.", x = names(percentile_values))
-    )
+    percentiles <- as.numeric(c(
+        gsub(pattern = "q_", replacement = "", x = names(lower_percentile)),
+        gsub(pattern = "q_", replacement = "", x = names(upper_percentile))
+    ))
+
+    # TODO: make use of lognormal or lnorm consistent
+    if (prob_dist == "lognormal") prob_dist <- "lnorm"
 
     # calculate the parameters from the percentiles
-    params <- extract_param(
+    prob_dist_params <- extract_param(
       type = "percentile",
       values = c(lower_percentile, upper_percentile),
       distribution = prob_dist,
       percentiles = percentiles
     )
-  } else if (length(median_range) == 3 && !is.null(sample_size)) {
-    params <- extract_param(
+  } else if (!any(is.na(median_range)) && !is.na(sample_size)) {
+    prob_dist_params <- extract_param(
       type = "range",
       values = median_range,
       distribution = prob_dist,
       samples = sample_size
     )
   } else {
-    message("No adequate summary statistics available to calculate the
-              parameters of the gamma distribution")
-    params <- NULL
+    message(paste0(
+      "No adequate summary statistics available to calculate the parameters ",
+      "of the ", prob_dist, " distribution"
+    ))
   }
 
   # return params
-  params
+  prob_dist_params
 }
 
 #' @export
