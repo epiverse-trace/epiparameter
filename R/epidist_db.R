f0d7b5f341abf183b0c76743d6447e18580bc37d
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-01-27 12:50:00 +0000

    added epidist_db() function and tests

---
+++ R/epidist_db.R
@@ -0,0 +1,71 @@
+#' Creates an `epidist` object directly from the epiparameter library (database)
+#'
+#' @description This function can extract an `epidist` object directly from the
+#' library of epidemiological parameters without having to read in an `epiparam`
+#' object and pull out an `epidist` object from one of the entries. If a
+#' distribution from a specific study is required, the `author` argument can
+#' be specified. For now this must match the author entry in the database
+#' exactly to be recognised.
+#'
+#' @details The current criteria for chosing a distribution when multiple
+#' entries exist in the database for a given disease and epidemiological
+#' distribution is to return the entry with the largest sample size.
+#'
+#' @param disease A character string specifying the disease
+#' @param epi_dist A character string specifying the epidemiological
+#' distribution
+#' @param author The author of the study reporting the distribution
+#'
+#' @return An `epidist` object
+#' @export
+#'
+#' @examples
+#' epidist_db(disease = "influenza", epi_dist = "serial_interval")
+epidist_db <- function(disease,
+                       epi_dist = c("incubation_period",
+                                    "onset_to_admission",
+                                    "onset_to_death",
+                                    "serial_interval",
+                                    "generation_time"),
+                       author = NULL) {
+
+  epi_dist <- match.arg(arg = epi_dist, several.ok = FALSE)
+
+  # read in database
+  eparam <- epiparam(epi_dist = epi_dist)
+
+  # match disease names against data
+  disease <- match.arg(
+    arg = disease,
+    choices = unique(eparam$disease),
+    several.ok = FALSE
+  )
+
+  # filter based on pathogen and delay distribution
+  eparam <- eparam[eparam$disease == disease, ]
+
+  # extract study or default to largest sample size
+  if (is.null(author)) {
+    eparam <- eparam[eparam$sample_size == max(eparam$sample_size), ]
+  } else {
+    study <- match.arg(
+      arg = author,
+      choices = unique(eparam$author),
+      several.ok = FALSE
+    )
+    eparam <- eparam[eparam$author == study, ]
+  }
+
+  # Throw warning if more than one row found and select first
+  if (nrow(eparam) > 1) {
+    warning("More than one study found. Selecting first one.
+    Please report an issue with duplicated studies.")
+    eparam <- eparam[1, ]
+  }
+
+  # convert epiparam to epidist
+  edist <- as_epidist(x = eparam)
+
+  # return epidist (or list of epidist)
+  edist
+}
---
+++ tests/testthat/test-epidist_db.R
@@ -0,0 +1,89 @@
+test_that("epidist_db works as expected", {
+  edist <- epidist_db(disease = "influenza", epi_dist = "serial_interval")
+
+  expect_s3_class(edist, class = "epidist")
+  expect_length(edist, 9)
+  expect_named(
+    edist,
+    c("disease", "epi_dist", "prob_dist", "uncertainty", "summary_stats",
+      "citation", "metadata", "method_assessment", "notes")
+  )
+  expect_type(edist$disease, "list")
+  expect_type(edist$epi_dist, "character")
+  expect_type(edist$prob_dist, "list")
+  expect_type(edist$uncertainty, "list")
+  expect_type(edist$summary_stats, "list")
+  expect_type(edist$citation, "character")
+  expect_type(edist$metadata, "list")
+  expect_type(edist$method_assessment, "list")
+  expect_type(edist$notes, "character")
+})
+
+test_that("epidist_db works as expected with author specified", {
+  edist <- epidist_db(
+    disease = "influenza",
+    epi_dist = "incubation_period",
+    author = "Ghani_etal"
+  )
+
+  expect_s3_class(edist, class = "epidist")
+  expect_length(edist, 9)
+  expect_named(
+    edist,
+    c("disease", "epi_dist", "prob_dist", "uncertainty", "summary_stats",
+      "citation", "metadata", "method_assessment", "notes")
+  )
+  expect_type(edist$disease, "list")
+  expect_type(edist$epi_dist, "character")
+  expect_type(edist$prob_dist, "list")
+  expect_type(edist$uncertainty, "list")
+  expect_type(edist$summary_stats, "list")
+  expect_type(edist$citation, "character")
+  expect_true(grepl(pattern = "Ghani", x = edist$citation))
+  expect_type(edist$metadata, "list")
+  expect_type(edist$method_assessment, "list")
+  expect_type(edist$notes, "character")
+})
+
+test_that("epidist_db fails as expected when author not recognised", {
+  expect_error(
+    epidist_db(
+      disease = "influenza",
+      epi_dist = "incubation_period",
+      author = "Lessler_et_al"
+    ),
+    regexp = "('arg' should be one of)"
+  )
+})
+
+test_that("epidist_db fails as expected when epi_dist not recognised", {
+  expect_error(
+    epidist_db(
+      disease = "influenza",
+      epi_dist = "onset_to_hospitalisation"
+    ),
+    regexp = "('arg' should be one of)"
+  )
+})
+
+test_that("epidist_db fails as expected with no entry in the database", {
+  expect_error(
+    epidist_db(
+      disease = "RSV",
+      epi_dist = "serial_interval"
+    ),
+    regexp = "('arg' should be one of)"
+  )
+})
+
+test_that("epidist_db throws warning as expected with multiple entries", {
+  expect_warning(
+    # message about stochastic optimisation suppressed
+    suppressMessages(epidist_db(
+      disease = "influenza",
+      epi_dist = "incubation_period",
+      author = "Reich_etal"
+    )),
+    regexp = "(More than one study found. Selecting first one)"
+  )
+})
