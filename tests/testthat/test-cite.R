4f62658b8332dc935dd1967807bb3408018cb560
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-04-04 16:13:12 +0100

    suppress message about citation in tests

--- tests/testthat/test-cite.R
+++ tests/testthat/test-cite.R
@@ -1,30 +1,37 @@
 test_that("get_citation works as expected for epidist from db", {
   eparam <- epiparam()
-  edist <- as_epidist(eparam[12, ])
+  # suppress message about citation
+  edist <- suppressMessages(as_epidist(eparam[12, ]))
   expect_type(get_citation(edist), "character")
 })
 
 test_that("get_citation works as expected for manual epidist", {
-  edist <- epidist(
-    disease = "ebola",
-    epi_dist = "incubation_period",
-    prob_distribution = "gamma",
-    prob_distribution_params = c(shape = 1, scale = 1),
-    citation = create_epidist_citation(
-      author = "Smith_etal",
-      year = 2000,
-      DOI = "10.19832/j.1366-9516.2012.09147.x"
+  # suppress message about citation
+  edist <- suppressMessages(
+    epidist(
+      disease = "ebola",
+      epi_dist = "incubation_period",
+      prob_distribution = "gamma",
+      prob_distribution_params = c(shape = 1, scale = 1),
+      citation = create_epidist_citation(
+        author = "Smith_etal",
+        year = 2000,
+        DOI = "10.19832/j.1366-9516.2012.09147.x"
+      )
     )
   )
   expect_type(get_citation(edist), "character")
 })
 
 test_that("get_citation works as expected for epidist missing citation", {
-  edist <- epidist(
-    disease = "ebola",
-    epi_dist = "incubation_period",
-    prob_distribution = "gamma",
-    prob_distribution_params = c(shape = 1, scale = 1)
+  # suppress message about citation
+  edist <- suppressMessages(
+    epidist(
+      disease = "ebola",
+      epi_dist = "incubation_period",
+      prob_distribution = "gamma",
+      prob_distribution_params = c(shape = 1, scale = 1)
+    )
   )
   citation <- get_citation(edist)
   expect_type(citation, "character")
