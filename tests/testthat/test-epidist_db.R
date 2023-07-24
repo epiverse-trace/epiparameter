cce31955ea9d8d2813f40caa019525827e67abe2
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-07-24 16:24:14 +0100

    added tests for new epidist_db subset and single_epidist arguments, WIP #91

--- tests/testthat/test-epidist_db.R
+++ tests/testthat/test-epidist_db.R
@@ -51,6 +51,64 @@ test_that("epidist_db works as expected with author specified", {
   expect_type(edist$notes, "character")
 })
 
+test_that("epidist_db works as expected with subsetting", {
+  # suppress message about citation
+  edist <- suppressMessages(
+    epidist_db(
+      disease = "influenza",
+      epi_dist = "incubation_period",
+      subset = sample_size == max(sample_size)
+    )
+  )
+
+  expect_s3_class(edist, class = "epidist")
+  expect_length(edist, 9)
+  expect_named(
+    edist,
+    c("disease", "epi_dist", "prob_dist", "uncertainty", "summary_stats",
+      "citation", "metadata", "method_assess", "notes")
+  )
+  expect_type(edist$disease, "list")
+  expect_type(edist$epi_dist, "character")
+  expect_type(edist$prob_dist, "character")
+  expect_type(edist$uncertainty, "list")
+  expect_type(edist$summary_stats, "list")
+  expect_type(edist$citation, "character")
+  expect_true(grepl(pattern = "Virlogeux", x = edist$citation, fixed = TRUE))
+  expect_type(edist$metadata, "list")
+  expect_type(edist$method_assess, "list")
+  expect_type(edist$notes, "character")
+})
+
+test_that("epidist_db works as expected with single_epidist as TRUE", {
+  # suppress message about citation
+  edist <- suppressMessages(
+    epidist_db(
+      disease = "influenza",
+      epi_dist = "incubation_period",
+      single_epidist = TRUE
+    )
+  )
+
+  expect_s3_class(edist, class = "epidist")
+  expect_length(edist, 9)
+  expect_named(
+    edist,
+    c("disease", "epi_dist", "prob_dist", "uncertainty", "summary_stats",
+      "citation", "metadata", "method_assess", "notes")
+  )
+  expect_type(edist$disease, "list")
+  expect_type(edist$epi_dist, "character")
+  expect_type(edist$prob_dist, "list")
+  expect_type(edist$uncertainty, "list")
+  expect_type(edist$summary_stats, "list")
+  expect_type(edist$citation, "character")
+  expect_true(grepl(pattern = "Ghani", x = edist$citation, fixed = TRUE))
+  expect_type(edist$metadata, "list")
+  expect_type(edist$method_assess, "list")
+  expect_type(edist$notes, "character")
+})
+
 test_that("epidist_db fails as expected when author not recognised", {
   expect_error(
     epidist_db(
@@ -82,17 +140,28 @@ test_that("epidist_db fails as expected with no entry in the database", {
   )
 })
 
-test_that("epidist_db throws warning as expected with multiple entries", {
+test_that("epidist_db gives message as expected with multiple entries", {
   # set seed for stochastic optimisation
   set.seed(123)
 
-  expect_warning(
+  expect_message(
     # message about stochastic optimisation suppressed
-    suppressMessages(epidist_db(
+    epidist_db(
       disease = "influenza",
       epi_dist = "incubation_period",
       author = "Reich_etal"
-    )),
-    regexp = "(More than one study found. Selecting first one)"
+    ),
+    regexp = "(Returning multiple studies that match the criteria)"
+  )
+})
+
+test_that("epidist_db fails as expected when subset given as string", {
+  expect_error(
+    epidist_db(
+      disease = "influenza",
+      epi_dist = "incubation_period",
+      subset = "author == 'Lessler_et_al'"
+    ),
+    regexp = "(Subsetting is done with expressions)"
   )
 })
