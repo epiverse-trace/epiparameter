0b7c594872b7844080c48c22abbf8046b79af280
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-09-10 22:13:25 +0100

    added release_bullets()

---
+++ R/dev-utils.R
@@ -0,0 +1,9 @@
+# This unexported function adds a custom item to `usethis::use_release_issue()`
+release_bullets <- function() {
+
+  c(
+    "Run `goodpractice::gp()`",
+    "Review [WORDLIST](https://docs.cran.dev/spelling#wordlist)"
+  )
+
+}
