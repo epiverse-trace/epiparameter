95bbcbf150cafc1661eebcf85005d97e6ea48f75
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-10-04 13:36:26 +0100

    rewrote list_distributions to work with new db and data structures, and updated documentation

--- R/list_distributions.R
+++ R/list_distributions.R
@@ -1,71 +1,90 @@
-#' List epidemiological distributions stored in an `epiparam` object
+#' List epidemiological distributions stored in the epiparameter library
 #'
-#' @description This function subsets an `epiparam` object to return only the
-#' chosen epidemiological distribution. The results are returned as a data
-#' frame to better see all of the returned distributions.
-#'
-#' By default the resulting data frame is subset to only return the disease,
-#' epidemiological distribution, probability distribution, author of the study
-#' and the year of publication as well as the sample size of the study. If the
-#' all columns of the database are required set `subset_db = FALSE`.
+#' @description This function subsets the epidemiological parameter library to
+#' return only the chosen epidemiological distribution. The results are
+#' returned as a data frame containing the disease, epidemiological
+#' distribution, probability distribution, author of the study, and the year
+#' of publication.
 #'
 #' @details The `<data.frame>` returned by [list_distributions()] contains some
 #' vector columns and some list columns. For example, the `author` column is a
 #' list as each paper can contain multiple authors.
 #'
-#' @inheritParams validate_epiparam
-#' @param epi_dist A character defining parameter to be listed:
-#' `"incubation"`, `"onset_to_hospitalisation"`, `"onset_to_death"`, or
-#' `"serial_interval"`. `"incubation_period"` is the default `epi_dist` so if no
-#' `epi_dist` is specified the incubation periods will be returned.
-#' @param subset_db A boolean logical that determines whether a subset, defaults
-#' is TRUE.
+#' @inheritParams epidist
+#' @param multi_epidist Either an `<epidist>` object or a list of `<epidist>`
+#' objects.
 #'
-#' @keywords distributions
 #' @author Adam Kucharski, Joshua W. Lambert
 #' @export
 #' @examples
-#' eparam <- epiparam()
-#' list_distributions(epiparam = eparam, epi_dist = "incubation_period")
-#' # the default for list_distributions() without any arguments is to return the
-#' # incubation period
-#' list_distributions(epiparam = eparam)
-#' # this same process can be achieved when loading the library
-#' eparam <- epiparam(epi_dist = "incubation_period")
+#' list_distributions(disease = "COVID-19", epi_dist = "incubation_period")
 #'
-#' # filtering for onset to death
-#' list_distributions(epiparam = eparam, epi_dist = "onset_to_death")
-list_distributions <- function(epiparam,
-                               epi_dist = c(
-                                 "incubation_period",
-                                 "onset_to_hospitalisation",
-                                 "onset_to_death",
-                                 "serial_interval",
-                                 "generation_time",
-                                 "offspring_distribution"
-                               ),
-                               subset_db = TRUE) {
-  # check input
-  validate_epiparam(epiparam)
-  epi_dist <- match.arg(arg = epi_dist, several.ok = FALSE)
-  checkmate::assert_logical(subset_db, len = 1)
-
-  # subset to chosen distribution
-  epiparam <- epiparam[epiparam$epi_distribution == epi_dist, ]
-
-  # strip epiparam class to return data frame
-  class(epiparam) <- "data.frame"
-
-  if (subset_db) {
-    epiparam <- epiparam[, c(
-      "disease", "epi_distribution", "prob_distribution", "author", "year",
-      "sample_size"
-    )]
+#' # example using list of `<epidist>` objects in memory
+#' epidist_lst <- epidist_db(disease = "COVID-19")
+#' list_distributions(multi_epidist = epidist_lst)
+#'
+#' # example filtering an existing list to incubation periods
+#' epidist_lst <- epidist_db(disease = "COVID-19")
+#' list_distributions(multi_epidist = epidist_lst, epi_dist = "incubation period")
+list_distributions <- function(disease = "all",
+                               epi_dist = "all",
+                               ...,
+                               multi_epidist) {
+  # read in data if not given
+  if (missing(multi_epidist)) {
+    multi_epidist <- epidist_db(disease = disease, epi_dist = epi_dist, ...)
   }
 
-  # reset indexing of rows
-  rownames(epiparam) <- NULL
+  # wrap <epidist> in list for apply functions
+  if (is_epidist(multi_epidist)) {
+    multi_epidist <- list(multi_epidist)
+  }
 
-  # return epiparam data frame
-  epiparam
+  # check data
+  stopifnot(
+    "List of <epidist> objects should be supplied to multi_epidist" =
+      all(vapply(multi_epidist, is_epidist, FUN.VALUE = logical(1))) &&
+        length(multi_epidist) != 0
+  )
+
+  multi_epidist <- .filter_epidist_db(
+    multi_epidist = multi_epidist,
+    disease = disease,
+    epi_dist = epi_dist
+  )
+
+  disease <- vapply(
+    multi_epidist,
+    function(x) x$disease$disease,
+    FUN.VALUE = character(1)
+  )
+  epi_dist <- vapply(
+    multi_epidist, "[[", "epi_dist",
+    FUN.VALUE = character(1)
+  )
+  prob_dist <- vapply(
+    multi_epidist, function(x) {
+      switch(class(x$prob_dist)[1],
+        "distcrete" = family(x),
+        "distribution" = family(x),
+        "character" = x$prob_dist,
+        "logical" = NA_character_
+      )
+    },
+    FUN.VALUE = character(1)
+  )
+  author <- lapply(multi_epidist, function(x) x$citation$author)
+  year <- vapply(
+    multi_epidist, function(x) as.numeric(x$citation$year),
+    FUN.VALUE = numeric(1)
+  )
+
+  # return data frame
+  data.frame(
+    disease = disease,
+    epi_distribution = epi_dist,
+    prob_distribution = prob_dist,
+    author = I(author),
+    year = year
+  )
 }
