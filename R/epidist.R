e1f1f01d4b2ed57273b59a11d0c4f1eaad7ceb8b
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-11-29 14:02:35 +0000

    added as.function method for <epidist>

--- R/epidist.R
+++ R/epidist.R
@@ -990,3 +990,45 @@ mean.epidist <- function(x, ...) {
   # return mean or NA
   mean
 }
+
+#' `as.function()` method for `<epidist>` class
+#'
+#' @description Converts an `<epidist>` object to a distribution function
+#' (see [epidist_distribution_functions]), either probability density/mass
+#' function, (`density`), cumulative distribution function (`cdf`), random
+#' number generator (`generate`), or quantile (`quantile`).
+#'
+#' @details The function returned takes a single required argument `x`.
+#'
+#' @inheritParams print.epidist
+#' @inheritParams base::as.function
+#' @param func_type A single `character` string specifying which distribution to
+#' convert `<epidist>` object into. Default is `"density"`. Other options are
+#' `"cdf"`, `"generate"`, or `"quantile"`.
+#'
+#' @return A [function] object.
+#' @export
+as.function.epidist <- function(x,
+                                func_type = c(
+                                  "density", "cdf", "generate", "quantile"
+                                ),
+                                ...) {
+  chkDots(...)
+  func_type <- match.arg(func_type)
+  if (!is_parameterised(x)) {
+    stop(
+      "Cannot convert unparameterised <epidist> to distribution function",
+      call. = FALSE
+    )
+  }
+  epidist <- x
+  x <- switch(func_type,
+    density = function(x) density(x = epidist, at = x),
+    cdf = function(x) epiparameter::cdf(x = epidist, q = x),
+    generate = function(x) epiparameter::generate(x = epidist, times = x),
+    quantile = function(x) quantile(x = epidist, p = x)
+  )
+
+  # return distribution function
+  x
+}
