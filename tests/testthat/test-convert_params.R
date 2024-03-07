656a848d384b535200e0772c6447aa4228ce99fa
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-03-07 15:56:12 +0000

    updated usage of conversion functions in tests and vignette

--- tests/testthat/test-convert_params.R
+++ tests/testthat/test-convert_params.R
@@ -1,8 +1,6 @@
 test_that("convert_params_to_summary_stats works as expected", {
   expect_equal(
-    convert_params_to_summary_stats(
-      distribution = "lnorm", meanlog = 1, sdlog = 0.5
-    ),
+    convert_params_to_summary_stats("lnorm", meanlog = 1, sdlog = 0.5),
     list(
       mean = 3.080217, median = 2.718282, mode = 2.117, var = 2.694758,
       sd = 1.641572, cv = 0.5329404, skewness = 1.75019, ex_kurtosis = 5.898446
@@ -11,9 +9,7 @@ test_that("convert_params_to_summary_stats works as expected", {
   )
 
   expect_equal(
-    convert_params_to_summary_stats(
-      distribution = "nbinom", prob = 0.5, dispersion = 1
-    ),
+    convert_params_to_summary_stats("nbinom", prob = 0.5, dispersion = 1),
     list(
       mean = 1, median = 0, mode = 0, var = 2, sd = 1.414214, cv = 1.414214,
       skewness = 2.12132, ex_kurtosis = 6.5
@@ -22,9 +18,7 @@ test_that("convert_params_to_summary_stats works as expected", {
   )
 
   expect_equal(
-    convert_params_to_summary_stats(
-      distribution = "gamma", shape = 1, scale = 1
-    ),
+    convert_params_to_summary_stats("gamma", shape = 1, scale = 1),
     list(
       mean = 1, median = 0.6931472, mode = 0, var = 1, sd = 1, cv = 1,
       skewness = 2, ex_kurtosis = 6
@@ -33,9 +27,7 @@ test_that("convert_params_to_summary_stats works as expected", {
   )
 
   expect_equal(
-    convert_params_to_summary_stats(
-      distribution = "weibull", shape = 1, scale = 1
-    ),
+    convert_params_to_summary_stats("weibull", shape = 1, scale = 1),
     list(
       mean = 1, median = 0.6931472, mode = 0, var = 1, sd = 1, cv = 1,
       skewness = 2, ex_kurtosis = 6
@@ -44,7 +36,7 @@ test_that("convert_params_to_summary_stats works as expected", {
   )
 
   expect_equal(
-    convert_params_to_summary_stats(distribution = "geom", prob = 0.5),
+    convert_params_to_summary_stats("geom", prob = 0.5),
     list(
       mean = 1, median = 0, mode = 0, var = 2, sd = 1.414214, cv = 1.414214,
       skewness = 2.12132, ex_kurtosis = 6.5
@@ -55,88 +47,68 @@ test_that("convert_params_to_summary_stats works as expected", {
 
 test_that("convert_params_to_summary_stats works as expected with dyn-dots", {
   expect_identical(
+    convert_params_to_summary_stats("lnorm", meanlog = 1, sdlog = 0.5),
+    convert_params_to_summary_stats("lnorm", !!!list(meanlog = 1, sdlog = 0.5))
+  )
+
+  expect_identical(
+    convert_params_to_summary_stats("nbinom", prob = 0.5, dispersion = 1),
     convert_params_to_summary_stats(
-      distribution = "lnorm", meanlog = 1, sdlog = 0.5
-    ),
-    convert_params_to_summary_stats(
-      distribution = "lnorm", !!!list(meanlog = 1, sdlog = 0.5)
+      "nbinom", !!!list(prob = 0.5, dispersion = 1)
     )
   )
 
   expect_identical(
-    convert_params_to_summary_stats(
-      distribution = "nbinom", prob = 0.5, dispersion = 1
-    ),
-    convert_params_to_summary_stats(
-      distribution = "nbinom", !!!list(prob = 0.5, dispersion = 1)
-    )
-  )
-
-  expect_identical(
-    convert_params_to_summary_stats(
-      distribution = "gamma", shape = 1, scale = 1
-    ),
-    convert_params_to_summary_stats(
-      distribution = "gamma", !!!list(shape = 1, scale = 1)
-    )
+    convert_params_to_summary_stats("gamma", shape = 1, scale = 1),
+    convert_params_to_summary_stats("gamma", !!!list(shape = 1, scale = 1))
   )
 
   expect_equal(
-    convert_params_to_summary_stats(
-      distribution = "weibull", shape = 1, scale = 1
-    ),
-    convert_params_to_summary_stats(
-      distribution = "weibull", !!!list(shape = 1, scale = 1)
-    )
+    convert_params_to_summary_stats("weibull", shape = 1, scale = 1),
+    convert_params_to_summary_stats("weibull", !!!list(shape = 1, scale = 1))
   )
 
   expect_equal(
-    convert_params_to_summary_stats(distribution = "geom", prob = 0.5),
-    convert_params_to_summary_stats(distribution = "geom", !!!list(prob = 0.5))
+    convert_params_to_summary_stats("geom", prob = 0.5),
+    convert_params_to_summary_stats("geom", !!!list(prob = 0.5))
   )
 })
 
 test_that("convert_summary_stats_to_params works as expected", {
   expect_equal(
-    convert_summary_stats_to_params(distribution = "gamma", mean = 1, sd = 0.5),
+    convert_summary_stats_to_params("gamma", mean = 1, sd = 0.5),
     list(shape = 4, scale = 0.25),
     tolerance = 1e-4
   )
 
   expect_equal(
     suppressMessages(
-      convert_summary_stats_to_params(
-        distribution = "weibull", mean = 0.5, cv = 1
-      )
+      convert_summary_stats_to_params("weibull", mean = 0.5, cv = 1)
     ),
     list(shape = 1.0, scale = 0.5),
     tolerance = 1e-3
   )
 
   expect_equal(
-    convert_summary_stats_to_params(distribution = "lnorm", median = 1, sd = 1),
+    convert_summary_stats_to_params("lnorm", median = 1, sd = 1),
     list(meanlog = -0.3465736, sdlog = 0.8325546),
     tolerance = 1e-4
   )
 
   expect_equal(
-    convert_summary_stats_to_params(
-      distribution = "nbinom", mean = 1, dispersion = 1
-    ),
+    convert_summary_stats_to_params("nbinom", mean = 1, dispersion = 1),
     list(prob = 0.5, dispersion = 1),
     tolerance = 1e-4
   )
 
   expect_equal(
-    convert_summary_stats_to_params(
-      distribution = "nbinom", mean = 1, sd = 1.5
-    ),
+    convert_summary_stats_to_params("nbinom", mean = 1, sd = 1.5),
     list(prob = 0.4444444, dispersion = 0.8),
     tolerance = 1e-4
   )
 
   expect_equal(
-    convert_summary_stats_to_params(distribution = "geom", mean = 0.5),
+    convert_summary_stats_to_params("geom", mean = 0.5),
     list(prob = 0.6666667),
     tolerance = 1e-4
   )
@@ -144,157 +116,128 @@ test_that("convert_summary_stats_to_params works as expected", {
 
 test_that("convert_summary_stats_to_params works as expected with dyn-dots", {
   expect_identical(
-    convert_summary_stats_to_params(distribution = "gamma", mean = 1, sd = 0.5),
-    convert_summary_stats_to_params(
-      distribution = "gamma", !!!list(mean = 1, sd = 0.5)
-    )
+    convert_summary_stats_to_params("gamma", mean = 1, sd = 0.5),
+    convert_summary_stats_to_params("gamma", !!!list(mean = 1, sd = 0.5))
   )
 
   expect_identical(
     suppressMessages(
-      convert_summary_stats_to_params(
-        distribution = "weibull", mean = 0.5, cv = 1
-      )
+      convert_summary_stats_to_params("weibull", mean = 0.5, cv = 1)
     ),
     suppressMessages(
-      convert_summary_stats_to_params(
-        distribution = "weibull", !!!list(mean = 0.5, cv = 1)
-      )
+      convert_summary_stats_to_params("weibull", !!!list(mean = 0.5, cv = 1))
     )
   )
 
   expect_identical(
-    convert_summary_stats_to_params(distribution = "lnorm", median = 1, sd = 1),
-    convert_summary_stats_to_params(
-      distribution = "lnorm", !!!list(median = 1, sd = 1)
+    convert_summary_stats_to_params("lnorm", median = 1, sd = 1),
+    convert_summary_stats_to_params("lnorm", !!!list(median = 1, sd = 1))
+  )
+
+  expect_identical(
+    convert_summary_stats_to_params("nbinom", mean = 1, dispersion = 1),
+    convert_summary_stats_to_params("nbinom", !!!list(mean = 1, dispersion = 1)
     )
   )
 
   expect_identical(
-    convert_summary_stats_to_params(
-      distribution = "nbinom", mean = 1, dispersion = 1
-    ),
-    convert_summary_stats_to_params(
-      distribution = "nbinom", !!!list(mean = 1, dispersion = 1)
-    )
+    convert_summary_stats_to_params("nbinom", mean = 1, sd = 1.5),
+    convert_summary_stats_to_params("nbinom", !!!list(mean = 1, sd = 1.5))
   )
 
   expect_identical(
-    convert_summary_stats_to_params(
-      distribution = "nbinom", mean = 1, sd = 1.5
-    ),
-    convert_summary_stats_to_params(
-      distribution = "nbinom", !!!list(mean = 1, sd = 1.5)
-    )
-  )
-
-  expect_identical(
-    convert_summary_stats_to_params(distribution = "geom", mean = 0.5),
-    convert_summary_stats_to_params(distribution = "geom", !!!list(mean = 0.5))
+    convert_summary_stats_to_params("geom", mean = 0.5),
+    convert_summary_stats_to_params("geom", !!!list(mean = 0.5))
   )
 })
 
 test_that("convert_params_to_summary_stats fails as expected", {
   expect_error(
-    convert_params_to_summary_stats(
-      distribution = "random", shape = 1, scale = 1
-    ),
+    convert_params_to_summary_stats("random", shape = 1, scale = 1),
     regexp = "(arg)*(should be one of)*(lnorm)*(gamma)"
   )
 
   expect_error(
-    convert_params_to_summary_stats(distribution = "lnorm"),
+    convert_params_to_summary_stats("lnorm"),
     regexp = "(Parameter(s) need to be named and supplied)*(to the function)"
   )
 
   expect_error(
-    convert_params_to_summary_stats(
-      distribution = "gamma", shape = -1, scale = 1
-    ),
+    convert_params_to_summary_stats("gamma", shape = -1, scale = 1),
     regexp = "(Assertion on)*(shape)*(failed: Element 1 is not >= 0.)"
   )
 
   expect_error(
-    convert_params_to_summary_stats(
-      distribution = "lnorm", shape = 1, scale = 1
-    ),
+    convert_params_to_summary_stats("lnorm", shape = 1, scale = 1),
     regexp = "(lnorm parameters must be named)*(meanlog)*(sdlog)"
   )
 
   expect_error(
-    convert_params_to_summary_stats(
-      distribution = "gamma", meanlog = 1, scale = 1
-    ),
+    convert_params_to_summary_stats("gamma", meanlog = 1, scale = 1),
     regexp = "(gamma parameters must be named)*(shape)*(scale)"
   )
 
   expect_error(
-    convert_params_to_summary_stats(
-      distribution = "weibull", meanlog = 1, scale = 1
-    ),
+    convert_params_to_summary_stats("weibull", meanlog = 1, scale = 1),
     regexp = "(weibull parameters must be named)*(shape)*(scale)"
   )
 
   expect_error(
     convert_params_to_summary_stats(
-      distribution = "nbinom", probability = 0.5, dispersion = 1
+      "nbinom", probability = 0.5, dispersion = 1
     ),
     regexp = "(nbinom parameters must be named)*(prob)*(dispersion)"
   )
 
   expect_error(
-    convert_params_to_summary_stats(distribution = "geom", probability = 0.5),
+    convert_params_to_summary_stats("geom", probability = 0.5),
     regexp = "(geom parameter must be named)*(prob)"
   )
 })
 
 test_that("convert_summary_stats_to_params fails as expected", {
   expect_error(
-    convert_summary_stats_to_params(distribution = "random", mean = 1, sd = 1),
+    convert_summary_stats_to_params("random", mean = 1, sd = 1),
     regexp = "(arg)*(should be one of)*(lnorm)*(gamma)"
   )
 
   expect_error(
-    convert_summary_stats_to_params(distribution = "lnorm"),
+    convert_summary_stats_to_params("lnorm"),
     regexp = "Summary statistics need to be named and supplied to the function"
   )
 
   expect_error(
-    convert_summary_stats_to_params(distribution = "gamma", mean = -1, sd = 1),
+    convert_summary_stats_to_params("gamma", mean = -1, sd = 1),
     regexp = "(Assertion on)*(x$mean)*(failed: Element 1 is not >= 0.)"
   )
 
   expect_error(
-    convert_summary_stats_to_params(distribution = "lnorm", mode = 1, var = 1),
+    convert_summary_stats_to_params("lnorm", mode = 1, var = 1),
     regexp = "Cannot calculate lognormal parameters from given input"
   )
 
   expect_error(
-    convert_summary_stats_to_params(distribution = "gamma", mode = 1, var = 1),
+    convert_summary_stats_to_params("gamma", mode = 1, var = 1),
     regexp = "Cannot calculate gamma parameters from given input"
   )
 
   expect_error(
-    convert_summary_stats_to_params(
-      distribution = "weibull", mode = 1, var = 1
-    ),
+    convert_summary_stats_to_params("weibull", mode = 1, var = 1),
     regexp = "Cannot calculate Weibull parameters from given input"
   )
 
   expect_error(
-    convert_summary_stats_to_params(distribution = "nbinom", mode = 1, var = 1),
+    convert_summary_stats_to_params("nbinom", mode = 1, var = 1),
     regexp = "(Cannot calculate negative binomial)*(parameters from)*(input)"
   )
 
   expect_error(
-    convert_summary_stats_to_params(
-      distribution = "nbinom", mean = 1, var = 0.5
-    ),
+    convert_summary_stats_to_params("nbinom", mean = 1, var = 0.5),
     regexp = "(Negative binomial)*(variance-to-mean ratio)*(greater than one)"
   )
 
   expect_error(
-    convert_summary_stats_to_params(distribution = "geom", mode = 1, var = 1),
+    convert_summary_stats_to_params("geom", mode = 1, var = 1),
     regexp = "(Cannot calculate geometric)*(parameter from given input)"
   )
 })
