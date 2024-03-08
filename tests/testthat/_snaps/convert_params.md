e1ec499e9f665b86a739dae86795d862e6d7a3fd
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-03-08 12:32:30 +0000

    updated convert_params tests and used snapshots for regression testing

---
+++ tests/testthat/_snaps/convert_params.md
@@ -0,0 +1,555 @@
+# convert_summary_stats_to_params.character works as expected
+
+    {
+      "type": "list",
+      "attributes": {
+        "names": {
+          "type": "character",
+          "attributes": {},
+          "value": ["shape", "scale"]
+        }
+      },
+      "value": [
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [4]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [0.25]
+        }
+      ]
+    }
+
+---
+
+    {
+      "type": "list",
+      "attributes": {
+        "names": {
+          "type": "character",
+          "attributes": {},
+          "value": ["shape", "scale"]
+        }
+      },
+      "value": [
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1.00001629]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [0.50000344]
+        }
+      ]
+    }
+
+---
+
+    {
+      "type": "list",
+      "attributes": {
+        "names": {
+          "type": "character",
+          "attributes": {},
+          "value": ["meanlog", "sdlog"]
+        }
+      },
+      "value": [
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [-0.34657359]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [0.83255461]
+        }
+      ]
+    }
+
+---
+
+    {
+      "type": "list",
+      "attributes": {
+        "names": {
+          "type": "character",
+          "attributes": {},
+          "value": ["prob", "dispersion"]
+        }
+      },
+      "value": [
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [0.5]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1]
+        }
+      ]
+    }
+
+---
+
+    {
+      "type": "list",
+      "attributes": {
+        "names": {
+          "type": "character",
+          "attributes": {},
+          "value": ["prob", "dispersion"]
+        }
+      },
+      "value": [
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [0.44444444]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [0.8]
+        }
+      ]
+    }
+
+---
+
+    {
+      "type": "list",
+      "attributes": {
+        "names": {
+          "type": "character",
+          "attributes": {},
+          "value": ["prob"]
+        }
+      },
+      "value": [
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [0.66666667]
+        }
+      ]
+    }
+
+# convert_summary_stats_to_params.epidist works as expected
+
+    {
+      "type": "list",
+      "attributes": {
+        "names": {
+          "type": "character",
+          "attributes": {},
+          "value": ["meanlog", "sdlog"]
+        }
+      },
+      "value": [
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [-0.34657359]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [0.83255461]
+        }
+      ]
+    }
+
+# convert_params_to_summary_stats.character works as expected
+
+    {
+      "type": "list",
+      "attributes": {
+        "names": {
+          "type": "character",
+          "attributes": {},
+          "value": ["mean", "median", "mode", "var", "sd", "cv", "skewness", "ex_kurtosis"]
+        }
+      },
+      "value": [
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [3.08021685]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [2.71828183]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [2.11700002]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [2.69475812]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1.64157185]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [0.53294035]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1.75018966]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [5.89844567]
+        }
+      ]
+    }
+
+---
+
+    {
+      "type": "list",
+      "attributes": {
+        "names": {
+          "type": "character",
+          "attributes": {},
+          "value": ["mean", "median", "mode", "var", "sd", "cv", "skewness", "ex_kurtosis"]
+        }
+      },
+      "value": [
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [0]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [0]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [2]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1.41421356]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1.41421356]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [2.12132034]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [6.5]
+        }
+      ]
+    }
+
+---
+
+    {
+      "type": "list",
+      "attributes": {
+        "names": {
+          "type": "character",
+          "attributes": {},
+          "value": ["mean", "median", "mode", "var", "sd", "cv", "skewness", "ex_kurtosis"]
+        }
+      },
+      "value": [
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [0.69314718]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [0]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [2]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [6]
+        }
+      ]
+    }
+
+---
+
+    {
+      "type": "list",
+      "attributes": {
+        "names": {
+          "type": "character",
+          "attributes": {},
+          "value": ["mean", "median", "mode", "var", "sd", "cv", "skewness", "ex_kurtosis"]
+        }
+      },
+      "value": [
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [0.69314718]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [0]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [2]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [6]
+        }
+      ]
+    }
+
+---
+
+    {
+      "type": "list",
+      "attributes": {
+        "names": {
+          "type": "character",
+          "attributes": {},
+          "value": ["mean", "median", "mode", "var", "sd", "cv", "skewness", "ex_kurtosis"]
+        }
+      },
+      "value": [
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [0]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [0]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [2]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1.41421356]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1.41421356]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [2.12132034]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [6.5]
+        }
+      ]
+    }
+
+# convert_params_to_summary_stats.epidist works as expected
+
+    {
+      "type": "list",
+      "attributes": {
+        "names": {
+          "type": "character",
+          "attributes": {},
+          "value": ["mean", "median", "mode", "var", "sd", "cv", "skewness", "ex_kurtosis"]
+        }
+      },
+      "value": [
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [4.48168907]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [2.71828183]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [34.51261311]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [5.87474366]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1.31083249]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [6.18487714]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [110.93639218]
+        }
+      ]
+    }
+
+---
+
+    {
+      "type": "list",
+      "attributes": {
+        "names": {
+          "type": "character",
+          "attributes": {},
+          "value": ["mean", "median", "mode", "var", "sd", "cv", "skewness", "ex_kurtosis"]
+        }
+      },
+      "value": [
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [4.48168907]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [2.71828183]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [34.51261311]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [5.87474366]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [1.31083249]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [6.18487714]
+        },
+        {
+          "type": "double",
+          "attributes": {},
+          "value": [110.93639218]
+        }
+      ]
+    }
+
--- tests/testthat/test-convert_params.R
+++ tests/testthat/test-convert_params.R
@@ -1,244 +1,201 @@
-test_that("convert_params_to_summary_stats works as expected", {
-  expect_equal(
-    convert_params_to_summary_stats("lnorm", meanlog = 1, sdlog = 0.5),
-    list(
-      mean = 3.080217, median = 2.718282, mode = 2.117, var = 2.694758,
-      sd = 1.641572, cv = 0.5329404, skewness = 1.75019, ex_kurtosis = 5.898446
-    ),
-    tolerance = 1e-4
+edist <- suppressMessages(
+  epidist(
+    disease = "Ebola",
+    epi_dist = "incubation period",
+    prob_distribution = "lnorm",
+    prob_distribution_params = c(meanlog = 1, sdlog = 1),
+    summary_stats = create_epidist_summary_stats(mean = 1, sd = 1)
   )
+)
 
-  expect_equal(
-    convert_params_to_summary_stats("nbinom", prob = 0.5, dispersion = 1),
-    list(
-      mean = 1, median = 0, mode = 0, var = 2, sd = 1.414214, cv = 1.414214,
-      skewness = 2.12132, ex_kurtosis = 6.5
-    ),
-    tolerance = 1e-4
-  )
-
-  expect_equal(
-    convert_params_to_summary_stats("gamma", shape = 1, scale = 1),
-    list(
-      mean = 1, median = 0.6931472, mode = 0, var = 1, sd = 1, cv = 1,
-      skewness = 2, ex_kurtosis = 6
-    ),
-    tolerance = 1e-4
-  )
-
-  expect_equal(
-    convert_params_to_summary_stats("weibull", shape = 1, scale = 1),
-    list(
-      mean = 1, median = 0.6931472, mode = 0, var = 1, sd = 1, cv = 1,
-      skewness = 2, ex_kurtosis = 6
-    ),
-    tolerance = 1e-4
-  )
-
-  expect_equal(
-    convert_params_to_summary_stats("geom", prob = 0.5),
-    list(
-      mean = 1, median = 0, mode = 0, var = 2, sd = 1.414214, cv = 1.414214,
-      skewness = 2.12132, ex_kurtosis = 6.5
-    ),
-    tolerance = 1e-4
-  )
-})
-
-test_that("convert_params_to_summary_stats works as expected with dyn-dots", {
-  expect_identical(
-    convert_params_to_summary_stats("lnorm", meanlog = 1, sdlog = 0.5),
-    convert_params_to_summary_stats("lnorm", !!!list(meanlog = 1, sdlog = 0.5))
-  )
-
-  expect_identical(
-    convert_params_to_summary_stats("nbinom", prob = 0.5, dispersion = 1),
-    convert_params_to_summary_stats(
-      "nbinom", !!!list(prob = 0.5, dispersion = 1)
-    )
-  )
-
-  expect_identical(
-    convert_params_to_summary_stats("gamma", shape = 1, scale = 1),
-    convert_params_to_summary_stats("gamma", !!!list(shape = 1, scale = 1))
-  )
-
-  expect_equal(
-    convert_params_to_summary_stats("weibull", shape = 1, scale = 1),
-    convert_params_to_summary_stats("weibull", !!!list(shape = 1, scale = 1))
-  )
-
-  expect_equal(
-    convert_params_to_summary_stats("geom", prob = 0.5),
-    convert_params_to_summary_stats("geom", !!!list(prob = 0.5))
-  )
-})
-
-test_that("convert_summary_stats_to_params works as expected", {
-  expect_equal(
+test_that("convert_summary_stats_to_params.character works as expected", {
+  expect_snapshot_value(
     convert_summary_stats_to_params("gamma", mean = 1, sd = 0.5),
-    list(shape = 4, scale = 0.25),
-    tolerance = 1e-4
+    style = "json2"
   )
-
-  expect_equal(
+  expect_snapshot_value(
     suppressMessages(
       convert_summary_stats_to_params("weibull", mean = 0.5, cv = 1)
     ),
-    list(shape = 1.0, scale = 0.5),
-    tolerance = 1e-3
+    style = "json2"
   )
-
-  expect_equal(
+  expect_snapshot_value(
     convert_summary_stats_to_params("lnorm", median = 1, sd = 1),
-    list(meanlog = -0.3465736, sdlog = 0.8325546),
-    tolerance = 1e-4
+    style = "json2"
   )
-
-  expect_equal(
+  expect_snapshot_value(
     convert_summary_stats_to_params("nbinom", mean = 1, dispersion = 1),
-    list(prob = 0.5, dispersion = 1),
-    tolerance = 1e-4
+    style = "json2"
   )
-
-  expect_equal(
+  expect_snapshot_value(
     convert_summary_stats_to_params("nbinom", mean = 1, sd = 1.5),
-    list(prob = 0.4444444, dispersion = 0.8),
-    tolerance = 1e-4
+    style = "json2"
   )
-
-  expect_equal(
+  expect_snapshot_value(
     convert_summary_stats_to_params("geom", mean = 0.5),
-    list(prob = 0.6666667),
-    tolerance = 1e-4
+    style = "json2"
   )
 })
 
-test_that("convert_summary_stats_to_params works as expected with dyn-dots", {
+test_that("convert_summary_stats_to_params.character works with dyn-dots", {
   expect_identical(
     convert_summary_stats_to_params("gamma", mean = 1, sd = 0.5),
     convert_summary_stats_to_params("gamma", !!!list(mean = 1, sd = 0.5))
   )
+})
 
-  expect_identical(
-    suppressMessages(
-      convert_summary_stats_to_params("weibull", mean = 0.5, cv = 1)
-    ),
-    suppressMessages(
-      convert_summary_stats_to_params("weibull", !!!list(mean = 0.5, cv = 1))
-    )
-  )
-
-  expect_identical(
-    convert_summary_stats_to_params("lnorm", median = 1, sd = 1),
-    convert_summary_stats_to_params("lnorm", !!!list(median = 1, sd = 1))
-  )
-
-  expect_identical(
-    convert_summary_stats_to_params("nbinom", mean = 1, dispersion = 1),
-    convert_summary_stats_to_params("nbinom", !!!list(mean = 1, dispersion = 1)
-    )
-  )
-
-  expect_identical(
-    convert_summary_stats_to_params("nbinom", mean = 1, sd = 1.5),
-    convert_summary_stats_to_params("nbinom", !!!list(mean = 1, sd = 1.5))
-  )
-
-  expect_identical(
-    convert_summary_stats_to_params("geom", mean = 0.5),
-    convert_summary_stats_to_params("geom", !!!list(mean = 0.5))
+test_that("convert_summary_stats_to_params.epidist works as expected", {
+  expect_snapshot_value(
+    convert_summary_stats_to_params(edist),
+    style = "json2"
   )
 })
 
-test_that("convert_params_to_summary_stats fails as expected", {
+test_that("convert_summary_stats_to_params.epidist warns as expected", {
+  expect_warning(
+    convert_summary_stats_to_params(edist, mean = 1, sd = 1),
+    regexp = "One or more summary statistics in <epidist> are being overwritten"
+  )
+})
+
+test_that("convert_summary_stats_to_params.character fails as expected", {
+  expect_error(
+    convert_summary_stats_to_params("random", mean = 1, sd = 1),
+    regexp = "(arg)*(should be one of)*(lnorm)*(gamma)"
+  )
+  expect_error(
+    convert_summary_stats_to_params("lnorm"),
+    regexp = "Summary statistics need to be named and supplied to the function"
+  )
+  expect_error(
+    convert_summary_stats_to_params("gamma", mean = -1, sd = 1),
+    regexp = "(Assertion on)*(x$mean)*(failed: Element 1 is not >= 0.)"
+  )
+  expect_error(
+    convert_summary_stats_to_params("lnorm", mode = 1, var = 1),
+    regexp = "Cannot calculate lognormal parameters from given input"
+  )
+  expect_error(
+    convert_summary_stats_to_params("gamma", mode = 1, var = 1),
+    regexp = "Cannot calculate gamma parameters from given input"
+  )
+  expect_error(
+    convert_summary_stats_to_params("weibull", mode = 1, var = 1),
+    regexp = "Cannot calculate Weibull parameters from given input"
+  )
+  expect_error(
+    convert_summary_stats_to_params("nbinom", mode = 1, var = 1),
+    regexp = "(Cannot calculate negative binomial)*(parameters from)*(input)"
+  )
+  expect_error(
+    convert_summary_stats_to_params("geom", mode = 1, var = 1),
+    regexp = "(Cannot calculate geometric)*(parameter from given input)"
+  )
+  expect_error(
+    convert_summary_stats_to_params("nbinom", mean = 1, var = 0.5),
+    regexp = "(Negative binomial)*(variance-to-mean ratio)*(greater than one)"
+  )
+})
+
+test_that("convert_summary_stats_to_params.epidist fails as expected", {
+  edist$summary_stats <- list()
+  expect_error(
+    convert_summary_stats_to_params(edist),
+    regexp = "Summary statistics need to be named and supplied to the function"
+  )
+})
+
+test_that("convert_params_to_summary_stats.character works as expected", {
+  expect_snapshot_value(
+    convert_params_to_summary_stats("lnorm", meanlog = 1, sdlog = 0.5),
+    style = "json2"
+  )
+  expect_snapshot_value(
+    convert_params_to_summary_stats("nbinom", prob = 0.5, dispersion = 1),
+    style = "json2"
+  )
+  expect_snapshot_value(
+    convert_params_to_summary_stats("gamma", shape = 1, scale = 1),
+    style = "json2"
+  )
+  expect_snapshot_value(
+    convert_params_to_summary_stats("weibull", shape = 1, scale = 1),
+    style = "json2"
+  )
+  expect_snapshot_value(
+    convert_params_to_summary_stats("geom", prob = 0.5),
+    style = "json2"
+  )
+})
+
+test_that("convert_params_to_summary_stats.character works with dyn-dots", {
+  expect_identical(
+    convert_params_to_summary_stats("lnorm", meanlog = 1, sdlog = 0.5),
+    convert_params_to_summary_stats("lnorm", !!!list(meanlog = 1, sdlog = 0.5))
+  )
+})
+
+test_that("convert_params_to_summary_stats.epidist works as expected", {
+  expect_snapshot_value(
+    convert_params_to_summary_stats(edist),
+    style = "json2"
+  )
+  edist <- "lnorm"
+  expect_snapshot_value(
+    convert_params_to_summary_stats(edist, meanlog = 1, sdlog = 1),
+    style = "json2"
+  )
+})
+
+test_that("convert_params_to_summary_stats.epidist warns as expected", {
+  expect_warning(
+    convert_params_to_summary_stats(edist, meanlog = 1, sdlog = 1),
+    regexp = "One or more parameters in <epidist> are being overwritten"
+  )
+})
+
+test_that("convert_params_to_summary_stats.character fails as expected", {
   expect_error(
     convert_params_to_summary_stats("random", shape = 1, scale = 1),
     regexp = "(arg)*(should be one of)*(lnorm)*(gamma)"
   )
-
   expect_error(
     convert_params_to_summary_stats("lnorm"),
     regexp = "(Parameter(s) need to be named and supplied)*(to the function)"
   )
-
   expect_error(
     convert_params_to_summary_stats("gamma", shape = -1, scale = 1),
     regexp = "(Assertion on)*(shape)*(failed: Element 1 is not >= 0.)"
   )
-
   expect_error(
     convert_params_to_summary_stats("lnorm", shape = 1, scale = 1),
     regexp = "(lnorm parameters must be named)*(meanlog)*(sdlog)"
   )
-
   expect_error(
     convert_params_to_summary_stats("gamma", meanlog = 1, scale = 1),
     regexp = "(gamma parameters must be named)*(shape)*(scale)"
   )
-
   expect_error(
     convert_params_to_summary_stats("weibull", meanlog = 1, scale = 1),
     regexp = "(weibull parameters must be named)*(shape)*(scale)"
   )
-
   expect_error(
     convert_params_to_summary_stats(
       "nbinom", probability = 0.5, dispersion = 1
     ),
     regexp = "(nbinom parameters must be named)*(prob)*(dispersion)"
   )
-
   expect_error(
     convert_params_to_summary_stats("geom", probability = 0.5),
     regexp = "(geom parameter must be named)*(prob)"
   )
 })
 
-test_that("convert_summary_stats_to_params fails as expected", {
+test_that("convert_params_to_summary_stats.epidist fails as expected", {
+  edist$prob_dist <- "lnorm"
   expect_error(
-    convert_summary_stats_to_params("random", mean = 1, sd = 1),
-    regexp = "(arg)*(should be one of)*(lnorm)*(gamma)"
-  )
-
-  expect_error(
-    convert_summary_stats_to_params("lnorm"),
-    regexp = "Summary statistics need to be named and supplied to the function"
-  )
-
-  expect_error(
-    convert_summary_stats_to_params("gamma", mean = -1, sd = 1),
-    regexp = "(Assertion on)*(x$mean)*(failed: Element 1 is not >= 0.)"
-  )
-
-  expect_error(
-    convert_summary_stats_to_params("lnorm", mode = 1, var = 1),
-    regexp = "Cannot calculate lognormal parameters from given input"
-  )
-
-  expect_error(
-    convert_summary_stats_to_params("gamma", mode = 1, var = 1),
-    regexp = "Cannot calculate gamma parameters from given input"
-  )
-
-  expect_error(
-    convert_summary_stats_to_params("weibull", mode = 1, var = 1),
-    regexp = "Cannot calculate Weibull parameters from given input"
-  )
-
-  expect_error(
-    convert_summary_stats_to_params("nbinom", mode = 1, var = 1),
-    regexp = "(Cannot calculate negative binomial)*(parameters from)*(input)"
-  )
-
-  expect_error(
-    convert_summary_stats_to_params("nbinom", mean = 1, var = 0.5),
-    regexp = "(Negative binomial)*(variance-to-mean ratio)*(greater than one)"
-  )
-
-  expect_error(
-    convert_summary_stats_to_params("geom", mode = 1, var = 1),
-    regexp = "(Cannot calculate geometric)*(parameter from given input)"
+    convert_params_to_summary_stats(edist),
+    regexp = "<epidist> supplied has no parameters and none are suppled"
   )
 })
 
@@ -246,11 +203,9 @@ test_that("get_sd works as expected", {
   x <- list(mean = 1, sd = 1)
   x <- get_sd(x)
   expect_identical(x, list(mean = 1, sd = 1))
-
   x <- list(mean = 1, var = 1)
   x <- get_sd(x)
   expect_identical(x, list(mean = 1, var = 1, sd = 1))
-
   x <- list(mean = 1, cv = 1)
   x <- get_sd(x)
   expect_identical(x, list(mean = 1, cv = 1, sd = 1))
@@ -259,31 +214,26 @@ test_that("get_sd works as expected", {
 test_that("chk_ss is working as expected", {
   x <- list(mean = 1, sd = 1)
   expect_no_error(chk_ss(x))
-
   x <- c(mean = 1, sd = 1)
   expect_error(
     chk_ss(x),
     regexp = "(Assertion)*(failed: Must be of type)*(list)*(double)"
   )
-
   x <- list(mean = 1)
   expect_error(
     chk_ss(x),
     regexp = "(Assertion)*(failed: Must have length)*(2)*(1)"
   )
-
   x <- list(mean = 1, 1)
   expect_error(
     chk_ss(x),
     regexp = "(Assertion)*(failed: Must have names)*(empty)"
   )
-
   x <- list(mean = 1, sd = "1")
   expect_error(
     chk_ss(x),
     regexp = "(Assertion)*(failed: May only contain)*(numeric)*(character)"
   )
-
   x <- list(mean = 1, ss = 1)
   expect_error(
     chk_ss(x),
