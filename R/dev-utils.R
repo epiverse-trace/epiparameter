3a890875296ac197659b6a309efafaf24807ace9
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-03-25 16:38:36 +0000

    added nolint and contributor points to release_bullets functions

--- R/dev-utils.R
+++ R/dev-utils.R
@@ -4,6 +4,8 @@ release_bullets <- function() { # nocov start
   c(
     "Run `goodpractice::gp()`",
     "Review [WORDLIST](https://docs.cran.dev/spelling#wordlist)",
+    "Check if `# nolint` comments are still needed with recent lintr releases",
+    "All contributors to this release are acknowledged in some way",
     "Update sysdata by running data-raw script"
   )
 
