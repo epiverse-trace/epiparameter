8f47cbf20221f30278b4296a48a550690c8fbcf8
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-05-24 10:22:03 +0100

    update day_range argument in plot.epidist to use NULL or vector of two numbers

--- R/plot.R
+++ R/plot.R
@@ -3,12 +3,12 @@
 #' @description Plot an `<epidist>` object by displaying the either the
 #' probability mass function (PMF), (in the case of discrete distributions)
 #' or probability density function (PDF) (in the case of continuous
-#' distributions) and the cumulative distribution function (CDF). Resulting in
-#' a 1x2 grid plot.
+#' distributions), or the cumulative distribution function (CDF).
 #'
 #' @param x An `<epidist>` object.
-#' @param day_range A vector with the sequence of days to be plotted on the
-#' x-axis of the distribution.
+#' @param day_range Either `NULL` or a `numeric` vector of length 2 with the
+#' first and last day to plot on the x-axis. If `NULL` the distribution is
+#' plotted between day 0 and the 99th quantile of the distribution.
 #' @param vb A boolean logical determining whether the `epidist` being plotted
 #' has come from a `vb_epidist` object.
 #' @param title Either a character string or `NULL`. If not null the character
@@ -26,31 +26,26 @@
 #'   prob_distribution = "gamma",
 #'   prob_distribution_params = c(shape = 2, scale = 1)
 #' )
+#' plot(edist)
 #'
-#' plot(edist, day_range = 0:10)
+#' # plot different day range (x-axis)
+#' plot(edist, day_range = c(0, 10))
 #'
 #' # plot CDF
-#' plot(edist, day_range = 0:10, cumulative = TRUE)
+#' plot(edist, cumulative = TRUE)
 #'
 #' # plot discrete epidist
-#' edist <- epidist(
-#'   disease = "ebola",
-#'   epi_dist = "incubation_period",
-#'   prob_distribution = "gamma",
-#'   prob_distribution_params = c(shape = 2, scale = 1),
-#'   discretise = TRUE
-#' )
-#'
-#' plot(edist, day_range = 0:10)
+#' edist <- discretise(edist)
+#' plot(edist)
 plot.epidist <- function(x,
-                         day_range = 0:10,
+                         day_range = NULL,
                          cumulative = FALSE,
                          ...,
                          vb = FALSE,
                          title = NULL) {
   # check input
   validate_epidist(x)
-  checkmate::assert_numeric(day_range, min.len = 2)
+  checkmate::assert_numeric(day_range, len = 2, null.ok = TRUE)
   checkmate::assert_logical(cumulative, any.missing = FALSE, len = 1)
 
   if (isFALSE(vb)) {
@@ -62,6 +57,12 @@ plot.epidist <- function(x,
     main <- "Probability Mass Function"
   } else {
     main <- "Probability Density Function"
+  }
+
+  if (is.null(day_range)) {
+    day_range <- seq(0, quantile(x, p = 0.99), length.out = 1000)
+  } else {
+    day_range <- seq(day_range[1], day_range[2], length.out = 1000)
   }
 
   if (cumulative) {
