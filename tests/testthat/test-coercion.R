09b196cb98e576e21b85b0d96478516d5d2b0bbc
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-06-13 15:33:58 +0100

    add unit test for as_epidist lasssa incubation period

--- tests/testthat/test-coercion.R
+++ tests/testthat/test-coercion.R
@@ -1,4 +1,4 @@
-test_that("as_epidist works for infectious period (issue #327)", {
+test_that("as_epidist works for ebola infectious period (issue #327 & #306)", {
   # {epireview} is not a dependency so only run if already on system
   skip_if_not_installed("epireview")
   # suppress warning and message about loading data
@@ -12,7 +12,7 @@ test_that("as_epidist works for infectious period (issue #327)", {
     ebola_params$parameter_type == "Human delay - infectious period",
   ]
   ebola_infectiousness <- ebola_infectiousness[
-    ebola_infectiousness$article_label == "Lau 2017 (1)",
+    !is.na(ebola_infectiousness$distribution_type),
   ]
   # suppress warning and message about citation
   ebola_infectiousness_epidist <- suppressWarnings(
@@ -28,4 +28,34 @@ test_that("as_epidist works for infectious period (issue #327)", {
   )
   # populate mean summary statistics
   expect_true(all(!is.na(ebola_infectiousness_epidist$summary_stats[1:3])))
+})
+
+test_that("as_epidist works for lassa incubation period (issue #306)", {
+  # {epireview} is not a dependency so only run if already on system
+  skip_if_not_installed("epireview")
+  # suppress warning and message about loading data
+  lassa_data <- suppressWarnings(
+    suppressMessages(
+      epireview::load_epidata("lassa")
+    )
+  )
+  lassa_params <- lassa_data$params
+  lassa_incub <- lassa_params[
+    which(lassa_params$article_label == "Akhmetzhanov 2019" &
+            lassa_params$parameter_type == "Human delay - incubation period"),
+  ]
+  # suppress warning and message about citation
+  lassa_incub_epidist <- suppressWarnings(
+    suppressMessages(
+      as_epidist(lassa_incub)
+    )
+  )
+  expect_s3_class(lassa_incub_epidist, class = "epidist")
+  # Akhmetzhanov 2019 has information to parameterise a gamma distribution
+  expect_s3_class(
+    lassa_incub_epidist$prob_dist,
+    class = "distribution"
+  )
+  # populate mean and sd summary statistics without uncertainty
+  expect_true(all(!is.na(lassa_incub_epidist$summary_stats[c("mean", "sd")])))
 })
