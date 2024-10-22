7bbb3f84ff8b8ad9e2a5c40091982917f6994064
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-10-22 16:29:32 +0100

    Add R-CMD-check workflow for R 4.1 (#405)
    * add R-CMD-check workflow for R 4.0
    
    * fix typo in R-CMD-check.yaml
    
    * skip print tests on R < 4.2.0 due to change in tools::Rd2txt

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -1,8 +1,13 @@
+# tests are skipped when R version < 4.2.0 due to a change in base tools::Rd2txt()
+# https://github.com/r-devel/r-svn/commit/aaabe42cc32582db77ba275233def72b11e32e45
+
 test_that("epiparameter_db print method works as expected for > 5 <epiparameter>", {
+  skip_if(getRversion() < "4.2.0")
   expect_snapshot(epiparameter_db())
 })
 
 test_that("epiparameter_db print method works as expected for <= 5 <epiparameter>", {
+  skip_if(getRversion() < "4.2.0")
   expect_snapshot(
     epiparameter_db(disease = "SARS", epi_name = "offspring distribution")
   )
