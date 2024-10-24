cd5c11e14326e6c936e90102108b176228e06a37
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-10-24 18:00:34 +0100

    add SARS to epireview_core_cols

--- R/data.R
+++ R/data.R
@@ -1,11 +1,9 @@
 #' A vector of `character` strings with the core column names of the
 #' epidemiological parameter data exported by the \pkg{epireview} R package.
 #'
-#' A subset of data from the World Health Organization Global Tuberculosis
-#' Report ...
-#'
 #' @format ## `epireview_core_cols`
-#' A `character` vector with 56 elements
+#' A `character` vector with `r length(epiparameter::epireview_core_cols)`
+#' elements.
 #'
 #' The data is taken as the intersection of the column names of each of the
 #' disease parameter tables in the \pkg{epireview} R package.
