1381dcdbdad2ab8eb666174094edd5b1f7f40ab8
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-11-22 10:48:47 +0000

    rm sysdata data-raw reminder from release_bullets

--- R/dev-utils.R
+++ R/dev-utils.R
@@ -6,7 +6,6 @@ release_bullets <- function() { # nocov start
     "Review [WORDLIST](https://docs.cran.dev/spelling#wordlist)",
     "Check if `# nolint` comments are still needed with recent lintr releases",
     "All contributors to this release are acknowledged in some way",
-    "Run data-raw/parameters.R script to update sysdata", # nolint nonportable_path_linter
     "Run vignettes/precompile.R to regenerate `extract-bias.Rmd` vignette" # nolint nonportable_path_linter
   )
 
