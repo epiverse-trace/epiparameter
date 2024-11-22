34e994072f89029b565768946e88064904c6c5cf
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-11-22 10:40:57 +0000

    rm parameters data-raw script

--- data-raw/parameters.R
+++
@@ -1,9 +0,0 @@
-## code to prepare `parameters` dataset goes here
-
-multi_epiparameter <- epiparameter:::.read_epiparameter_db()
-usethis::use_data(
-  multi_epiparameter,
-  internal = TRUE,
-  overwrite = TRUE,
-  version = 3
-)
