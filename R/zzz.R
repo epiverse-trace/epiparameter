6b4d8a3bc394f1fc81af34a5f3fe7af8f4ebc997
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-11-22 15:57:43 +0000

    create empty env and cache DB in .onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,15 @@
+# package environment for storing global objects for caching
+cache_env <- new.env(parent = emptyenv())
+
 .onLoad <- function(libname, pkgname) {
   options(epiparameter = epiparameter_options) # nolint undersirable_function_linter
+
+  # create an in-memory cache
+  cache_env$cache <- cachem::cache_mem()
+
+  # suppress DB loading message on package loading
+  db <- suppressMessages(epiparameter_db())
+
+  # store epiparameter library in cache
+  cache_env$cache$set("multi_epiparameter", db)
 }
