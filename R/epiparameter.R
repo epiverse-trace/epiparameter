381a3dad86f4772a0f9a1985ee373932c3a4c551
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2025-02-04 14:07:00 +0000

    apply offset in <epiparameter> distribution functions

--- R/epiparameter.R
+++ R/epiparameter.R
@@ -504,6 +504,7 @@ density.epiparameter <- function(x, at, ...) {
   if (isFALSE(is_parameterised(x))) {
     stop("<epiparameter> is unparameterised", call. = FALSE)
   }
+  at <- at - attr(x$prob_distribution, "offset")
   if (inherits(x$prob_distribution, "distcrete")) {
     out <- x$prob_distribution$d(at)
   } else {
@@ -526,6 +527,7 @@ cdf.epiparameter <- function(x, q, ..., log = FALSE) {
   if (isFALSE(is_parameterised(x))) {
     stop("<epiparameter> is unparameterised", call. = FALSE)
   }
+  q <- q - attr(x$prob_distribution, "offset")
   if (inherits(x$prob_distribution, "distcrete")) {
     out <- x$prob_distribution$p(q)
     if (log) out <- log(out)
@@ -550,10 +552,11 @@ quantile.epiparameter <- function(x, p, ...) {
   } else {
     out <- stats::quantile(x$prob_distribution, p = p)
   }
+  offset <- attr(x$prob_distribution, "offset")
   if (is.atomic(out)) {
-    return(out)
+    return(out + offset)
   }
-  return(unlist(out, recursive = FALSE))
+  return(unlist(out, recursive = FALSE) + offset)
 }
 
 #' @importFrom distributional generate
@@ -578,6 +581,7 @@ generate.epiparameter <- function(x, times, ...) {
     out <- distributional::generate(x$prob_distribution, times = times)
     out <- unlist(out, recursive = recursive)
   }
+  out <- out + attr(x$prob_distribution, "offset")
   out
 }
 
